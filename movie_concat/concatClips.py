0126b84b268a370f55d64ee6e29a545f2ef9ab7d
Dominic Gaiero <dpgaiero@gmail.com>
2019-08-26 20:21:13 -0700

    added movie processing

--- movie_concat/concatClips.py
+++ movie_concat/concatClips.py
@@ -1,1 +1,129 @@
-from moviepy.editor import VideoFileClip
+import datetime
+import io
+import json
+import os
+import shutil
+import subprocess
+import sys
+import time
+from configparser import ConfigParser
+from io import StringIO
+
+import click
+import cv2
+import sendgrid
+import numpy as np
+from moviepy.editor import ImageClip, VideoFileClip, concatenate_videoclips, TextClip
+from PIL import Image, ImageDraw, ImageFont
+from sendgrid.helpers.mail import Content, Email, Mail
+
+
+class VideoProcessor:
+
+    def __init__(self, config_location):
+        # Setup settings configuration for settings.ini file
+        settings_config = ConfigParser()
+        settings_config.read(config_location)
+        # Mail Settings
+        self.send_mail = settings_config.get('mail_settings', 'send_mail')
+        self.sendgrid_API = settings_config.get(
+            'mail_settings', 'sendgrid_API')
+        self.from_email = settings_config.get('mail_settings', 'from_email')
+        self.to_email = settings_config.get('mail_settings', 'to_email')
+        # Directory Settings
+        self.current_directory = os.path.join(os.path.dirname(os.getcwd()), "handbrake_util")
+        # Movie Directory Settings
+        self.movie_input_directory = settings_config.get(
+            'directory_settings', 'movie_input_directory')
+        self.copy_to_plex = settings_config.get(
+            'directory_settings', 'copy_to_plex')
+        self.movie_file_extensions = json.loads(settings_config.get(
+            'directory_settings', 'movie_file_extensions'))
+        self.plex_home_video_directory = settings_config.get(
+            'directory_settings', 'plex_home_video_directory')
+        # print(json.loads((self.movie_file_extensions)))
+        self.movie_file_dir_list = find_dirs(self.movie_input_directory)
+        self.files = []
+
+    def list_dirs(self):
+        return self.movie_file_dir_list
+
+    def get_files(self, dir_name):
+        pathName = os.path.join(self.movie_input_directory,dir_name)
+        files = walklevel(pathName, 5)
+        rootDirFileList = list(files)
+        fileList = []
+        for item in rootDirFileList:
+            for file in item[2]:
+                if not(file.lower().endswith(tuple(self.movie_file_extensions))):
+                    continue
+                fileList += [f"{item[0]}\{file}"]
+        fileDateList = []
+        for item in fileList:
+            fileDateList.append((item, os.path.getmtime(item)))
+        fileDateList.sort(key=lambda file: os.path.getctime(file[0]))
+        self.files = fileDateList
+        return fileDateList
+
+    def concat(self, folder):
+        start_time = time.clock()
+        self.createTitleSlide(folder)
+        filesToConcat = self.get_files(folder)
+        filesToConcat = list(map(lambda file: VideoFileClip(file[0]), filesToConcat))
+        # filesToConcat=  []
+        titleSlide = ImageClip(f"{self.movie_input_directory}temp.PNG", duration=3)
+        filesToConcat =  [titleSlide] + filesToConcat
+        final_clip = concatenate_videoclips(filesToConcat)
+        final_clip.write_videofile(f"{self.movie_input_directory}/{folder}.mp4")
+        end_time = time.clock()
+        os.remove(f"{self.movie_input_directory}temp.PNG")
+        print(f"done in {str(datetime.timedelta(seconds=end_time - start_time))}")
+        # print(filesToConcat)
+
+    def copyOutputFile(self, folder):
+        try:
+            os.makedirs(os.path.join(self.plex_home_video_directory,folder))
+        except FileExistsError:
+            print(f"{self.plex_home_video_directory}/{folder} already exists.")
+        shutil.move(f"{self.movie_input_directory}/{folder}.mp4",
+                    f"{self.plex_home_video_directory}/{folder}/{folder}.mp4")
+
+    def createTitleSlide(self, folder):
+        W, H = (1920, 1080)
+        msg = folder
+        im = Image.new("RGBA", (W, H), "black")
+        draw = ImageDraw.Draw(im)
+        dfont = ImageFont.truetype(font="arial", size=int(48))
+        w, h = draw.textsize(msg, font=dfont)
+        draw.text(((W-w)/2, (H-h)/2), msg, fill="white", font=dfont)
+        im.save(f"{self.movie_input_directory}temp.PNG", "PNG")
+
+    def send_email(self, body, subject):
+        sg = sendgrid.SendGridAPIClient(apikey=self.sendgrid_API)
+        from_email = Email(self.from_email)
+        to_email = Email(self.to_email)
+        content = Content("text/html", body)
+        mail = Mail(from_email, subject, to_email, content)
+        sg.client.mail.send.post(request_body=mail.get())
+
+
+
+def find_dirs(root_dir):
+    file_list = []
+    for root, dirs, files in walklevel(root_dir):
+        file_name = root.replace(root_dir, "")
+        file_list.append((root, file_name))
+
+    return file_list[1:]
+
+
+def walklevel(some_dir, level=1):
+    """ Same as os.walk with level command """
+    some_dir = some_dir.rstrip(os.path.sep)
+    assert os.path.isdir(some_dir)
+    num_sep = some_dir.count(os.path.sep)
+    for root, dirs, files in os.walk(some_dir):
+        yield root, dirs, files
+        num_sep_this = root.count(os.path.sep)
+        if num_sep + level <= num_sep_this:
+            del dirs[:]
--- server/server.py
+++ server/server.py
@@ -3,10 +3,15 @@ import sys
 from io import StringIO
 
 from celery import Celery
+from celery.signals import worker_process_init
+from multiprocessing import current_process
+
 from flask import Flask, render_template, request, Response, stream_with_context
+import time
 
 sys.path.append(os.path.dirname(os.getcwd()))
 import handbrake_util.handbrake_plex_encode
+import movie_concat.concatClips
 
 
 app = Flask(__name__)
@@ -20,7 +25,7 @@ def index():
     links = ""
     for rule in app.url_map.iter_rules():
         if rule.endpoint != "static":
-            links += "<a href='{0}{1}'>{0}{1}</a>\n".format(request.base_url, rule)
+            links += "<a href='{0}{1}'>{0}{1}</a>\n".format(request.base_url[0:-1], rule)
     links += "</pre>"
     return("<pre>The following are valid rules. Either click the one below or navigate to the URL directly.\n\n" + links)
     # return("Please navigate to the script URL provided to you.")
@@ -82,8 +87,87 @@ def handbrake_proc_runner():
 
 @app.route('/movie-proc')
 def movie_processor():
-    return "test"
+    movie_proc_dir = os.path.join(os.path.dirname(
+        os.getcwd()), "movie_concat")
+    vProc = movie_concat.concatClips.VideoProcessor(
+        os.path.join(movie_proc_dir, "settings.ini"))
+    
 
+    movie_dirs = "<pre><b>Chose a directory below:</b>\n"
+    folder = request.args.get('dir')
+    print(request.args)
+    if folder:
+        if request.args.get('start'):
+            if os.path.isfile(os.path.join(movie_proc_dir, ".lockfile")):
+                return("<pre>A video is being processed. You must wait for it to finish before submitting a new job.</pre>")
+            else:
+                open(os.path.join(movie_proc_dir, ".lockfile"), "w")
+                movie_proc_runner.delay(folder)
+                return ("<pre>You may now close this window. On completion, the script will finish and an email will be sent to the recipients in the configuration file.</pre>")
+
+        else:
+            if vProc.get_files(folder) == []:
+                return f"<pre>It appears that there are no files which match the file type criteria ({vProc.movie_file_extensions}). Double check that you have the right files.</pre>"
+            else:
+                returnStr = f"<pre>Current directory <b>{vProc.movie_input_directory}{folder}</b> (<a href='{request.base_url}'>Go back to directory selection</a>).\n"
+                returnStr += f"The output file will be located in {vProc.movie_input_directory}{folder}.mp4\n"
+                if vProc.copy_to_plex:
+                    returnStr += f"Copy to plex is enabled, so the output file will be copied to the plex home videos directory.\n"
+                returnStr += f"Only the following extensions will be processed: {vProc.movie_file_extensions}. To change this, edit the settings.ini file.\n"
+                returnStr += "\n<em>The following files will be processed:</em>\n"
+                for file in vProc.get_files(folder):
+                    returnStr += f"* {file[0]} ({time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(file[1]))})\n"
+                returnStr += f"<b>To continue, click <a href='{request.base_url}?dir={folder}&start=true'>here</a></b>"
+                return returnStr + "</pre>"
+
+    else:
+        for folder in vProc.list_dirs():
+            movie_dirs += "* <a href='{0}?dir={1}'>{1}</a> ({2})\n".format(
+                request.base_url, folder[1], folder[0])
+        return movie_dirs + "</pre>"
+
+
+@worker_process_init.connect
+def fix_multiprocessing(**kwargs):
+    try:
+        current_process()._config
+    except AttributeError:
+        current_process()._config = {'semprefix': '/mp'}
+
+@celery.task
+def movie_proc_runner(folder):
+    ''' Handles processing of moves and handoff to handbrake-util script.
+        Redirects stdout if email flag is set to true in vProc. Returns plain text to display to end user on webpage '''
+    movie_proc_dir = os.path.join(os.path.dirname(
+        os.getcwd()), "movie_concat")
+    try:
+        vProc = movie_concat.concatClips.VideoProcessor(
+        os.path.join(movie_proc_dir, "settings.ini"))
+        if vProc.send_mail:
+            old_stdout = sys.stdout
+            sys.stdout = proc_stdout = StringIO()
+        print("<pre>")
+        print(f"Processing folder: {vProc.movie_input_directory}{folder}")
+        print("\n<em>The following files will be processed:</em>")
+        for file in vProc.get_files(folder):
+            print(f"* {file[0]} ({time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(file[1]))})")
+        print(f"The output file will be located in {vProc.movie_input_directory}{folder}.mp4")
+        vProc.concat(folder)
+        if vProc.copy_to_plex:
+            print(f"Copy to plex is enabled, the output file will be copied to the plex home videos directory.")
+            vProc.copyOutputFile(folder)
+        else:
+            print(f"Copy to plex is disabled, the output file will not be copied to the plex home videos directory.")
+        if vProc.send_mail:
+            print("</pre>")
+            sys.stdout = old_stdout
+            email = proc_stdout.getvalue()
+            print(email)
+            vProc.send_email(email, f"{vProc.movie_input_directory}{folder}.mp4 finished processing")
+        os.remove(os.path.join(movie_proc_dir, ".lockfile"))
+    except Exception as e:
+        os.remove(os.path.join(movie_proc_dir, ".lockfile"))
+        raise Exception(e)
 
 if __name__ == "__main__":
     ''' For debug purposes only. Set host to 0.0.0.0 to allow access on all NICs.'''
